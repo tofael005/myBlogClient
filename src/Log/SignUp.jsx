0606011b39350f1c108262847670cb767a58743d
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-26 23:40:00 +0600

    register

--- src/Log/SignUp.jsx
+++ src/Log/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SignUp = () => {
     return (
         <div className="max-w-[1240px] mx-auto bg-slate-300 h-[90vh]">
-            <h1>Loging Page!</h1>
+            <h1>Loging Page</h1>
             <form className="w-[600px] mx-auto md:mt-28">
                 <label htmlFor="" className="text-lg">Name:</label><br />
                 <input className="w-full border p-3 rounded-md mt-1 mb-4" type="text" name="name" id="" placeholder="Enter name" /><br />
@@ -14,7 +14,7 @@ const SignUp = () => {
                 <input className="w-full border p-3 rounded-md mt-1" type="password" name="password" id="" placeholder="Enter password" />
 
                 <input className="bg-blue-400 w-full block mx-auto p-3 rounded-md mt-5" type="button" value="Sign Up" />
-                <p className="text-center mt-3">Already have an account.<span className="text-blue-800"><Link to="/login">Login</Link></span></p>
+                <p className="text-center mt-3">Already have an account<span className="text-blue-800"><Link to="/login">Login</Link></span></p>
             </form>
         </div>
     );
