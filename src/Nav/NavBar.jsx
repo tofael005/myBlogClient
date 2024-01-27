ab02dcd81709bdf4db6b72901040eeaf2f855349
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-01-27 20:04:31 +0600

    navs

--- src/Nav/NavBar.jsx
+++ src/Nav/NavBar.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div className="flex justify-between max-w-[1240px] mx-auto">
+        <div className="flex justify-between max-w-[1240px] mx-auto py-3">
             <h1>NavBar!</h1>
 
             <ul>
                 <li>
-                    <Link to="/login">Login!</Link>
+                    <Link to="/login"></Link>
                 </li>
+                
             </ul>
         </div>
     );
