a6393293beabebfdc2cc55c7a90478d3803438d8
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-02-01 22:43:44 +0600

    blog pages edite

--- src/components/RecentBlog/RecentBlog.jsx
+++ src/components/RecentBlog/RecentBlog.jsx
@@ -2,8 +2,8 @@
 
 const RecentBlog = () => {
     return (
-        <div className="max-w-[1240px] mx-auto mt-10">
-            <h1>Recent Blogs</h1>
+        <div className="max-w-[1240px] mx-auto mt-10 px-3">
+            <h1>Recent Blog</h1>
         </div>
     );
 };
