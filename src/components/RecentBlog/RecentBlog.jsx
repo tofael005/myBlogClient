462e17bb63ebe8469d3ad7bbe687213542e0f1de
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-02-17 23:08:18 +0600

    arch

--- src/components/RecentBlog/RecentBlog.jsx
+++ src/components/RecentBlog/RecentBlog.jsx
@@ -3,7 +3,7 @@
 const RecentBlog = () => {
     return (
         <div className="max-w-[1240px] mx-auto mt-10 px-3">
-            <h1>Recents Blog.</h1>
+            <h1>Recents Blog..</h1>
         </div>
     );
 };
