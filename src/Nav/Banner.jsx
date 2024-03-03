bf3598726a0579e6db759f1d861dcb5a967ff2c6
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-03 23:37:33 +0600

    max add

--- src/Nav/Banner.jsx
+++ src/Nav/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div>
             <img src={banner} alt="" />
             <h1>hdjsdjs.........</h1>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, hic saepe inventore facilis voluptatem excepturi temporibus quos provident sit voluptate possimus iusto, libero quaerat dolorum unde at quo eaque. Blanditiis quaerat quibusdam eius mollitia voluptate sapiente accusantium sed id repellendus ex, placeat omnis dicta nisi labore, dolorem numquam quis laudantiums.,,,,</h1>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, hic saepe inventore facilis voluptatem excepturi temporibus quos provident sit voluptate possimus iusto, libero quaerat dolorum unde at quo eaque. Blanditiis quaerat quibusdam eius mollitia voluptate sapiente accusantium sed id repellendus ex, placeat omnis dicta nisi labore, dolorem numquam quis laudantiums.,,,,.</h1>
         </div>
     );
 };
