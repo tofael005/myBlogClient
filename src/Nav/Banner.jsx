f409b974f210f45377ee2880cf134d3e80d31b1b
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-11 23:17:08 +0600

    beated

--- src/Nav/Banner.jsx
+++ src/Nav/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
             <h1>hdjsdjs.........</h1>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, hic saepe inventore facilis voluptatem excepturi temporibus quos provident sit voluptate possimus iusto, libero quaerat dolorum unde at quo eaque. Blanditiis quaerat quibusdam eius mollitia voluptate sapiente accusantium sed id repellendus ex, placeat omnis dicta nisi labore, dolorem numquam quis laudantiums.,,,,...</h1>
             <h1>......</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.. Beatae,distinctio?...</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.. Beatae,distinctio?....</p>
         </div>
     );
 };
