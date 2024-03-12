1e8541620f0b4336201e9299790be094ebc00526
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-12 23:49:02 +0600

    Home left

--- src/Pages/LandingContent/LandingSection.jsx
+++ src/Pages/LandingContent/LandingSection.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+
 
 const LandingSection = () => {
     return (
         <div>
-            <h1></h1>
+            <div>
+                <div>
+                    <h1>Left</h1>
+                </div>
+            </div>
         </div>
     );
 };
