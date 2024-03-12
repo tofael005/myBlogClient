1ed0852704d8a9a1f25cc21566dc8132265939d4
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-12 23:58:51 +0600

    left img ok

--- src/Pages/LandingContent/LandingSection.jsx
+++ src/Pages/LandingContent/LandingSection.jsx
@@ -1,11 +1,11 @@
-
+import about from "../../assets/banner/me.jpg"
 
 const LandingSection = () => {
     return (
         <div className="mt-12 container mx-auto">
             <div className="flex md:flex-row flex-col gap-6">
                 <div className="md:w-[400px]">
-                    <h1>Left</h1>
+                    <img src={about} alt="" />
                 </div>
 
                 <div className="md:w-[800px]">
