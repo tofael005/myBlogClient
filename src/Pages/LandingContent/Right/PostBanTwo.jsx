fdeec756006265e2e035d1a14437057ca35d8caf
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-20 00:07:28 +0600

    image one import

--- src/Pages/LandingContent/Right/PostBanTwo.jsx
+++ src/Pages/LandingContent/Right/PostBanTwo.jsx
@@ -1,11 +1,11 @@
-
+import postOne from "../../../assets/post/Beach.jpeg"
 
 const PostBanTwo = () => {
     return (
         <div>
             <h1>Post</h1>
             <div>
-                
+                <img src={postOne} alt="" />
             </div>
         </div>
     );
