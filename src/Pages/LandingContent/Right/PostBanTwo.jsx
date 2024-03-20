23c37ff83e5e7127d21591c1b55034bb2f6cec68
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-21 00:26:37 +0600

    fist post edited

--- src/Pages/LandingContent/Right/PostBanTwo.jsx
+++ src/Pages/LandingContent/Right/PostBanTwo.jsx
@@ -1,14 +1,46 @@
+import { FaComments, FaLocationArrow } from "react-icons/fa6";
 import postOne from "../../../assets/post/Beach.jpeg"
 import postTwo from "../../../assets/post/car beach.jpeg"
 
 
 const PostBanTwo = () => {
     return (
-        <div>
-            <h1>Post</h1>
-            <div>
-                <img src={postOne} alt="" />
-                <img src={postTwo} alt="" />
+        <div className="mt-14">
+            <div className="grid md:grid-cols-2 gap-6">
+                <div className="shadow-black/20 shadow-sm rounded-md">
+                    <img className="md:w-[430px] rounded-t-md" src={postOne} alt="" />
+                    <div className="p-3">
+                        <p>September 17, 2021 - Tips & Tricks</p>
+                        <h1 className="text-3xl font-semibold my-6">Have you read The Beach by Alex?</h1>
+                        <div className="flex justify-between items-center mt-14 mb-3">
+                            <div className="flex justify-between items-center gap-2">
+                                <FaLocationArrow />
+                                <p>Panang, Malaysia</p>
+                            </div>
+                            <div className="flex justify-between items-center gap-2">
+                                <FaComments />
+                                <p>Comment (52)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="shadow-black/20 shadow-sm rounded-md">
+                    <img className="w-full md:w-[430px] rounded-t-md" src={postTwo} alt="" />
+                    <div className="p-3">
+                        <p>September 17, 2021 - Tips & Tricks</p>
+                        <h1 className="text-3xl font-semibold my-6">The writer actually live in Philippines</h1>
+                        <div className="flex justify-between items-center mt-14 mb-3">
+                            <div className="flex justify-between items-center gap-2">
+                                <FaLocationArrow />
+                                <p>Panang, Malaysia</p>
+                            </div>
+                            <div className="flex justify-between items-center gap-2">
+                                <FaComments />
+                                <p>Comment (52)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
