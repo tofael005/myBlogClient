f11e1873217022de1722657bccc870b47e1de407
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-03-30 00:04:30 +0600

    mountains

--- src/Pages/LandingContent/Left/Left.jsx
+++ src/Pages/LandingContent/Left/Left.jsx
@@ -28,7 +28,7 @@ const Left = () => {
                         <p>(22)</p>
                     </div>
                     <div className="flex justify-between items-center my-2 p-2 rounded-md hover:shadow-blue-400 hover:shadow-md duration-300 cursor-pointer">
-                        <h1>Mountains</h1>
+                        <h1>Mountains.</h1>
                         <p>(22)</p>
                     </div>
                     <div className="flex justify-between items-center my-2 p-2 rounded-md hover:shadow-blue-400 hover:shadow-md duration-300 cursor-pointer">
