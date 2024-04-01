c47f6773587bf8b1199bff7012809867e0975972
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-01 22:49:11 +0600

    post three imga add

--- src/Pages/LandingContent/Right/PostBanThree.jsx
+++ src/Pages/LandingContent/Right/PostBanThree.jsx
@@ -1,9 +1,10 @@
-
+import post from "../../../assets/post/car beach.jpeg"
 
 const PostBanThree = () => {
     return (
         <div>
             <h1>Post Banner three</h1>
+            <img src="" alt="" />
         </div>
     );
 };
