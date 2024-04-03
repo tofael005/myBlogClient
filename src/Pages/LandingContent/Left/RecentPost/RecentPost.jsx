d9a5f5bc32e6f91f43200c341f28f81d015b671f
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-04 00:02:37 +0600

    correction

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -3,7 +3,7 @@
 const RecentPost = () => {
     return (
         <div className="border rounded-md shadow-black/25 shadow-sm mt-10">
-            <h1 className="text-2xl font-bold text-center p-4">Recent Post</h1>
+            <h1 className="text-2xl font-bold text-center p-4">Recent.. Post</h1>
         </div>
     );
 };
