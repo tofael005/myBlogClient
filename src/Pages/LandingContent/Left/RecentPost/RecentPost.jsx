53bed4dccce6d7e02a34febb4b6be890f7ef2095
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-04 00:09:08 +0600

    recent post added

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -1,9 +1,16 @@
-
+import post from "../../../../assets/post/Beach.jpeg"
 
 const RecentPost = () => {
     return (
-        <div className="border rounded-md shadow-black/30 shadow-sm mt-10">
-            <h1 className="text-2xl font-bold text-center p-4">Recent Post</h1>
+        <div>
+            <div className="border rounded-md shadow-black/30 shadow-sm mt-10">
+                <h1 className="text-2xl font-bold text-center p-4">Recent Post</h1>
+            </div>
+            <div>
+                <div>
+                    <img src={post} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
