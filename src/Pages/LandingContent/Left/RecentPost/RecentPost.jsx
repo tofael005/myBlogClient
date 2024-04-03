d80d4bf270721fa14730c26401a8dd0abf0d47ef
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-04 00:11:49 +0600

    post sec

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -6,7 +6,7 @@ const RecentPost = () => {
             <div className="border rounded-md shadow-black/30 shadow-sm mt-10">
                 <h1 className="text-2xl font-bold text-center p-4">Recent Post</h1>
             </div>
-            <div>
+            <div className="mt-10">
                 <div>
                     <img src={post} alt="" />
                 </div>
