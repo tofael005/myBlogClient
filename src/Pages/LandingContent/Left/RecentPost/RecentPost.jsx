efadd418a5c207d3722eb54720d005bec39d1fbc
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-06 00:35:32 +0600

    edited

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -21,7 +21,7 @@ const RecentPost = () => {
                 <div className="flex gap-3 items-center">
                     <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt="" />
                     <div>
-                        <p className="text-sm">September 17, 2021 - Tips & Tricks</p>
+                        <p className="text-sm">September 17, 2021 - Tips & Tricks.</p>
                         <h1 className="text-xl mt-2 font-bold">Have you read The Beach by Alex.</h1>
                     </div>
                 </div>
