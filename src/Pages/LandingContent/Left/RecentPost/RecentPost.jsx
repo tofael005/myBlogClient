093da948f29b2c0582bd8d4fd3cc63221c767a37
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-05 17:49:26 +0600

    some edited

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -6,12 +6,32 @@ const RecentPost = () => {
             <div className="border rounded-md shadow-black/30 shadow-sm mt-10">
                 <h1 className="text-2xl font-bold text-center p-4">Recent Post</h1>
             </div>
+
+            
             <div className="mt-10">
                 <div className="flex gap-3 items-center">
-                    <img src={post} alt="" />
+                    <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt="" />
                     <div>
-                        <p>September 17, 2021 - Tips & Tricks</p>
-                        <h1>Have you read The Beach by Alex.</h1>
+                        <p className="text-sm">September 17, 2021 - Tips & Tricks</p>
+                        <h1 className="text-xl mt-2 font-bold">Have you read The Beach by Alex.</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-6">
+                <div className="flex gap-3 items-center">
+                    <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt="" />
+                    <div>
+                        <p className="text-sm">September 17, 2021 - Tips & Tricks</p>
+                        <h1 className="text-xl mt-2 font-bold">Have you read The Beach by Alex.</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-6">
+                <div className="flex gap-3 items-center">
+                    <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt="" />
+                    <div>
+                        <p className="text-sm">September 17, 2021 - Tips & Tricks</p>
+                        <h1 className="text-xl mt-2 font-bold">Have you read The Beach by Alex.</h1>
                     </div>
                 </div>
             </div>
