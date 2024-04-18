1c5e421faf007b93d0e7f38a296f74cfc73c8c16
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-04-18 08:02:10 +0600

    img edite

--- src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
+++ src/Pages/LandingContent/Left/RecentPost/RecentPost.jsx
@@ -10,7 +10,7 @@ const RecentPost = () => {
             
             <div className="mt-10">
                 <div className="flex gap-3 items-center">
-                    <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt="" />
+                    <img className="md:w-[100px] h-[80px] rounded-md" src={post} alt=""/>
                     <div>
                         <p className="text-sm">September 17, 2021 - Tips & Tricks.</p>
                         <h1 className="text-xl mt-2 font-bold">Have you read The Beach by Alex.</h1>
