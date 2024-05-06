531e71954fda54ee0a9d42186ce8b560117bad54
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-05-07 00:14:46 +0600

    image resiz

--- src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
+++ src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
@@ -7,9 +7,9 @@ const ProductThat = () => {
             <h1 className="text-center p-4 font-bold hover:text-blue-500">Products That I, Have</h1>
         </div>
 
-        <div>
-            <div>
-                <img src={product} alt="" />
+        <div className="mt-6">
+            <div className="flex justify-center items-center gap-6">
+                <img className="w-[100px] h-[100px]" src={product} alt="" />
                 <div>
                     <h1>Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
                     <p>$514</p>
