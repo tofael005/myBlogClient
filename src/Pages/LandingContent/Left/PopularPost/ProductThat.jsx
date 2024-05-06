bb86f1c0de382d1fad1f97dec4ec666848177ba1
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-05-07 00:09:33 +0600

    new categ addded

--- src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
+++ src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
@@ -2,8 +2,15 @@
 
 const ProductThat = () => {
     return (
-        <div className="mt-10 border rounded-md shadow-sm shadow-black/20 cursor-pointer">
+        <div>
+            <div className="mt-10 border rounded-md shadow-sm shadow-black/20 cursor-pointer">
             <h1 className="text-center p-4 font-bold hover:text-blue-500">Products That I, Have</h1>
+        </div>
+
+        <div>
+            
+        </div>
+
         </div>
     );
 };
