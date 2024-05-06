beb26015093c9a4f52a1b7ea46b3e9e6d0dc4370
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-05-07 00:07:29 +0600

    text customize

--- src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
+++ src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
@@ -3,7 +3,7 @@
 const ProductThat = () => {
     return (
         <div className="mt-10 border rounded-md shadow-sm shadow-black/20 cursor-pointer">
-            <h1 className="text-center p-4 font-bold">Products That I, Have</h1>
+            <h1 className="text-center p-4 font-bold hover:text-blue-500">Products That I, Have</h1>
         </div>
     );
 };
