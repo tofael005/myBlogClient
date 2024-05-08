c161ff6ea1f06604beeae076cd59dc131c392a31
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-05-08 21:18:42 +0600

    product price

--- src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
+++ src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
@@ -4,15 +4,29 @@ const ProductThat = () => {
     return (
         <div>
             <div className="mt-10 border rounded-md shadow-sm shadow-black/20 cursor-pointer">
-            <h1 className="text-center p-4 font-bold hover:text-blue-500">Products That I, Have</h1>
+            <h1 className="text-center p-4 font-bold hover:text-blue-500 duration-300">Products That I, Have</h1>
         </div>
 
         <div className="mt-6">
             <div className="flex justify-center items-center gap-6">
-                <img className="w-[100px] h-[100px]" src={product} alt="" />
+                <img className="w-[100px] h-[100px] rounded-md" src={product} alt="" />
                 <div>
-                    <h1 className="text-xl font-semibold">Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
-                    <p>$514</p>
+                    <h1 className="text-lg font-semibold mb-5 cursor-pointer">Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
+                    <p className="text-blue-500 font-semibold">$514</p>
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-6 my-6">
+                <img className="w-[100px] h-[100px] rounded-md" src={product} alt="" />
+                <div>
+                    <h1 className="text-lg font-semibold mb-5 cursor-pointer">Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
+                    <p className="text-blue-500 font-semibold">$514</p>
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-6">
+                <img className="w-[100px] h-[100px] rounded-md" src={product} alt="" />
+                <div>
+                    <h1 className="text-lg font-semibold mb-5 cursor-pointer">Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
+                    <p className="text-blue-500 font-semibold">$514.</p>
                 </div>
             </div>
         </div>
