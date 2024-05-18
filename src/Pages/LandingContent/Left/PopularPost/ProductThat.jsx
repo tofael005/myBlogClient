ad2f7fbb8615d3ab3e106307eb4a1b2816146a22
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-05-18 17:50:55 +0600

    ss bd

--- src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
+++ src/Pages/LandingContent/Left/PopularPost/ProductThat.jsx
@@ -4,7 +4,7 @@ const ProductThat = () => {
     return (
         <div>
             <div className="mt-10 border rounded-md shadow-sm shadow-black/20 cursor-pointer">
-            <h1 className="text-center p-4 font-bold hover:text-blue-500 duration-300">Products That I, Have</h1>
+            <h1 className="text-center p-4 font-bold hover:text-blue-500 duration-300">Product That I, Have</h1>
         </div>
 
         <div className="mt-6">
@@ -12,7 +12,7 @@ const ProductThat = () => {
                 <img className="w-[100px] h-[100px] rounded-md" src={product} alt="" />
                 <div>
                     <h1 className="text-lg font-semibold mb-5 cursor-pointer">Apple MacBook Air  MJV2ELL/A 13-inch Laptop </h1>
-                    <p className="text-blue-500 font-semibold">$514.</p>
+                    <p className="text-blue-500 font-semibold">$514</p>
                 </div>
             </div>
             <div className="flex justify-center items-center gap-6 my-6">
